78c5fbff8748d45551c2b99048e7f9881e08a041
Anthony Lubic <alubic@celerity.com>
2017-06-05 09:07:02 -0400

    broke out the scenarios into separate functions and was able to pass all unit tests, still think it could be done quicker and more elegantly though

--- solutions/ch01/1.5/oneAway.js
+++ solutions/ch01/1.5/oneAway.js
@@ -1,27 +1,37 @@
 function oneAway(str1, str2) {
-	let changes = countInserts(str1, str2);
-	changes += countDeletes(str1, str2);
-	if (changes > 1) return false;
+	if (str1.length === str2.length) return needsOneReplace(str1, str2);
+	if (str1.length + 1 === str2.length) return needsOneInsert(str1, str2);
+	//just reverse the arg order, dont need to write a delete
+	if (str1.length - 1 === str2.length) return needsOneInsert(str2, str1);
+	return false;
+}
 
-	for (let i = str1.length; i > 0; i--) {
-		console.log(str1, str2);
-		let pos = str2.indexOf(str1[i]);
-		str1 = str1.slice(0, i) + str1.slice(i + 1, str1.length);
-		if (pos === -1) continue;
-		str2 = str2.slice(0, pos) + str2.slice(pos + 1, str2.length);
+function needsOneInsert(str1, str2) {
+	let i = 0;
+	let j = 0;
+	let mulligan = false;
+	while (i < str1.length && j < str2.length) {
+		if (str1[i] !== str2[j]) {
+			if (mulligan) return false;
+			mulligan = true;
+			j++;
+		} else {
+			i++;
+			j++;
+		}
 	}
-
-	return changes <= 1;
+	return true;
 }
 
-function countInserts(str1, str2) {
-	let diff = str1.length - str2.length;
-	return diff < 0 ? 0 : diff;
-}
-
-function countDeletes(str1, str2) {
-	let diff = str2.length - str1.length;
-	return diff < 0 ? 0 : diff;
+function needsOneReplace(str1, str2) {
+	let mulligan = false;
+	for (let i = 0; i < str1.length; i++) {
+		if (str1[i] !== str2[i]) {
+			if (mulligan) return false;
+			mulligan = true;
+		}
+	}
+	return true;
 }
 
 module.exports = oneAway;
