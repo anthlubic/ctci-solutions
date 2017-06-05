6f7f55d220a0cd565457cdec6deb68a2ee6785d7
Anthony Lubic <alubic@celerity.com>
2017-06-05 11:15:43 -0400

    wrote failing test for 1.7 rotatematrix

---
+++ solutions/ch01/1.7/rotateMatrix.js
@@ -0,0 +1,12 @@
+/*
+1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+bytes, write a method to rotate the image by 90 degrees. Can you do this in place? 
+
+My note: I cannot do it in place easily, so I'm going to construct a new matrix and return that instead.
+*/
+
+function rotateMatrix(matrix) {
+	return matrix;
+}
+
+module.exports = rotateMatrix;
