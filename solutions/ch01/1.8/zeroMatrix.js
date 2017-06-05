6e531540a5f0d3a63cc295b7dd1f81c3ec536b82
Anthony Lubic <alubic@celerity.com>
2017-06-05 11:34:40 -0400

    add 1.8 passing test but it fails on certain scenarios

---
+++ solutions/ch01/1.8/zeroMatrix.js
@@ -0,0 +1,40 @@
+/*
+1.8 Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+column are set to 0.
+*/
+
+/*
+  My mental notes:
+  loop through each row in the matrix
+
+    - if we find a zero
+      - add to a zero-tracking hashset
+      - zero the whole row
+      - move onto next row
+    - if this index is in the zero-tracking hashset
+      - set to zero and move on
+  //this fails if theres multiple zeroes in a row.
+  //need to finish out the row and rely on the zeroes set??
+*/
+
+function zeroMatrix(matrix) {
+	let zeroes = new Set();
+	for (let row = 0; row < matrix.length; row++) {
+		let currentRow = matrix[row];
+		for (let col = 0; col < currentRow.length; col++) {
+			if (currentRow[col] === 0) {
+				zeroes.add(col);
+				for (let i = 0; i < currentRow.length; i++) {
+					currentRow[i] = 0;
+				}
+				break;
+			}
+			if (zeroes.has(col)) {
+				currentRow[col] = 0;
+			}
+		}
+	}
+	return matrix;
+}
+
+module.exports = zeroMatrix;
---
+++ solutions/ch01/1.8/zeroMatrix.spec.js
@@ -0,0 +1,16 @@
+const zeroMatrix = require('./zeroMatrix');
+
+describe('1.7 - zeroMatrix', () => {
+	it('should zero the row and col when it finds a zero', () => {
+		let input = [[0, 1, 2], [5, 1, 3], [9, 5, 5]];
+		let output = zeroMatrix(input);
+		let expected = [[0, 0, 0], [0, 1, 3], [0, 5, 5]];
+		expect(expected).toEqual(output);
+	});
+	it('should handle multiple zeroes in a row', () => {
+		let input = [[0, 1, 0], [5, 1, 3], [9, 5, 5]];
+		let output = zeroMatrix(input);
+		let expected = [[0, 0, 0], [0, 1, 0], [0, 5, 0]];
+		expect(expected).toEqual(output);
+	});
+});
