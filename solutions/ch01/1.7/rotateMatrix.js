3fc81638604f5a46ff81c921c87ec5658b38e531
Anth Lubic <anthlubic@gmail.com>
2017-06-06 21:17:17 -0400

    check-in package-lock.json and finish rotateMatrix, god damn i hated that one

--- solutions/ch01/1.7/rotateMatrix.js
+++ solutions/ch01/1.7/rotateMatrix.js
@@ -1,11 +1,34 @@
 /*
 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
-bytes, write a method to rotate the image by 90 degrees. Can you do this in place? 
-
-My note: I cannot do it in place easily, so I'm going to construct a new matrix and return that instead.
+bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 */
 
 function rotateMatrix(matrix) {
+	//matrix has to be an array of arrays and it must be NxN.
+	if (!Array.isArray(matrix) || matrix.length === 0 || matrix.length !== matrix[0].length) {
+		throw new Error('Matrix must be an array of arrays and be NxN (a square)');
+	}
+
+	let size = matrix.length;
+
+	for (let layer = 0; layer < size / 2; ++layer) {
+		let first = layer;
+		let last = size - 1 - layer;
+		
+		for (let i = first; i < last; ++i) {
+			let offset = i - first;
+			let top = matrix[first][i];
+
+			matrix[first][i] = matrix[last - offset][first];
+
+			matrix[last - offset][first] = matrix[last][last - offset];
+
+			matrix[last][last - offset] = matrix[i][last];
+
+			matrix[i][last] = top;
+		}
+	}
+
 	return matrix;
 }
 
--- solutions/ch01/1.7/rotateMatrix.spec.js
+++ solutions/ch01/1.7/rotateMatrix.spec.js
@@ -1,10 +1,34 @@
 const rotateMatrix = require('./rotateMatrix');
 
 describe('1.7 - rotateMatrix', () => {
-	// it('should rotate clockwise', () => {
-	// 	let input = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']];
-	// 	let output = rotateMatrix(input);
-	// 	let expected = [['G', 'D', 'A'], ['H', 'E', 'B'], ['I', 'F', 'C']];
-	// 	expect(expected).toEqual(output);
-	// });
+	it('should rotate odd-n matrices clockwise', () => {
+		let input = [
+			['A', 'B', 'C'], 
+			['D', 'E', 'F'], 
+			['G', 'H', 'I']
+		];
+		let output = rotateMatrix(input);
+		let expected = [
+			['G', 'D', 'A'], 
+			['H', 'E', 'B'], 
+			['I', 'F', 'C']
+		];
+		expect(expected).toEqual(output);
+	});
+	it('should rotate even-n matrices clockwise', () => {
+		let input = [
+			[1, 2, 3, 4], 
+			[5, 6, 7, 8], 
+			[9, 10, 11, 12],
+			[13, 14, 15, 16]
+		];
+		let output = rotateMatrix(input);
+		let expected = [
+			[13, 9, 5, 1],
+			[14, 10, 6, 2],
+			[15, 11, 7, 3],
+			[16, 12, 8, 4]
+		];
+		expect(expected).toEqual(output);
+	});
 });
